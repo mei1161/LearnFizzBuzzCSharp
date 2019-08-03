49d6af5fc8571c9336eed01bcd94371ee22989dd
芽衣 武田 <ichigo_happy@ezweb.ne.jp>
2019-08-03 22:42:15 +0900

    コンソールに出力する。

---
+++ LearnFizzBuzzCSharp.Entry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LearnFizzBuzzCSharp.Entry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var i in FizzBuzz.GetNum())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
